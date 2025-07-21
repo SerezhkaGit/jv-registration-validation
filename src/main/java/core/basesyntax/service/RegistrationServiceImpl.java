b2211ed4a1faceb3fec199d3a357497e0f5c8d4f
Serhiy <sergiy.kusiv@gmail.com>
2025-07-21 18:27:33 +0300

    Realised requirements from ai mentor

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -1,7 +1,6 @@
 package core.basesyntax.service;
 
-import core.basesyntax.dao.StorageDao;
-import core.basesyntax.dao.StorageDaoImpl;
+import core.basesyntax.db.Storage;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
@@ -9,25 +8,37 @@ public class RegistrationServiceImpl implements RegistrationService {
     private static final int MIN_PASSWORD_LENGTH = 6;
     private static final int MIN_AGE = 18;
 
-    private final StorageDao storageDao = new StorageDaoImpl();
-
     @Override
     public User register(User user) {
         if (user == null) {
-            throw new RegistrationException("User cannot be null");
+            throw new RegistrationException("User should not be null");
         }
-        if (user.getLogin() == null || user.getLogin().length() < MIN_LOGIN_LENGTH) {
+        if (user.getLogin() == null) {
+            throw new RegistrationException("Login should not be null");
+        }
+        if (user.getLogin().length() < MIN_LOGIN_LENGTH) {
             throw new RegistrationException("Login should be at least 6 characters long");
         }
-        if (user.getPassword() == null || user.getPassword().length() < MIN_PASSWORD_LENGTH) {
+        if (user.getPassword() == null) {
+            throw new RegistrationException("Password should not be null");
+        }
+        if (user.getPassword().length() < MIN_PASSWORD_LENGTH) {
             throw new RegistrationException("Password should be at least 6 characters long");
         }
-        if (user.getAge() == null || user.getAge() < MIN_AGE) {
-            throw new RegistrationException("User must be at least 18 years old");
+        if (user.getAge() == null) {
+            throw new RegistrationException("Age should not be null");
         }
-        if (storageDao.get(user.getLogin()) != null) {
-            throw new RegistrationException("User with this login already exists");
+        if (user.getAge() < MIN_AGE) {
+            throw new RegistrationException("User should be at least 18 years old");
         }
-        return storageDao.add(user);
+
+        for (User existing : Storage.people) {
+            if (existing.getLogin().equals(user.getLogin())) {
+                throw new RegistrationException("Login is already in use");
+            }
+        }
+
+        Storage.people.add(user);
+        return user;
     }
 }
