bd903046030b226f6a1e66f02dcabf1bc62bea98
Serhiy <sergiy.kusiv@gmail.com>
2025-07-21 18:30:02 +0300

    Realised requirements from ai mentor 2

--- src/main/java/core/basesyntax/dao/StorageDaoImpl.java
+++ src/main/java/core/basesyntax/dao/StorageDaoImpl.java
@@ -8,22 +8,6 @@ public class StorageDaoImpl implements StorageDao {
 
     @Override
     public User add(User user) {
-        if (user.getLogin().length() < 6) {
-            throw new RuntimeException("Login should be longer than 6 characters");
-        }
-        if (user.getPassword().length() < 6) {
-            throw new RuntimeException("Password should be longer than 6 characters");
-        }
-        if (user.getAge() < 18) {
-            throw new RuntimeException("Only for users after 18 years old :(");
-        }
-
-        for (User person : Storage.people) {
-            if (person.getLogin().equals(user.getLogin())) {
-                throw new RuntimeException("There is already a user with such Login");
-            }
-        }
-
         user.setId(++index);
         Storage.people.add(user);
         return user;
@@ -39,3 +23,4 @@ public class StorageDaoImpl implements StorageDao {
         return null;
     }
 }
+
